57a6ba763f7fefccf1bdbd197fcd13f1e5e41f9d
Error404-code <christianmpho@gmail.com>
2022-03-14 00:09:39 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {ToDo} from "./ToDo";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoList-angular';
+
+  todoValue :string = "";
+  list:ToDo[] =[];
+  
+  //lifeCycle hook
+  ngOnInit(): void {
+    this.list = [];
+    this.todoValue=""
+    
+  }
+
+  //add functionality
+
+  addItem = ()=>{
+    if(this.todoValue !=="")
+     {//set object of type ToDo
+        const newItem:ToDo= {
+          id:Date.now(),
+          value:this.todoValue,
+          isDone:false
+        }
+        //add object to list array
+        this.list.push(newItem);
+        this.todoValue="";
+     }
+
+  }
+
+  deleteItem =(id:number)=>{
+      this.list= this.list.filter((item)=> item.id !==id);
+  }
+
+
+
 }
